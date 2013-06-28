58b7b1dece7f289d104fa42c155aa66f5bd583e6
tong <tong@disktree.net>
2013-06-28 13:49:42 +0200

    added sha1

---
+++ src/hxssl_sha1.cpp
@@ -0,0 +1,23 @@
+
+#include <hx/CFFI.h>
+#include <string.h>
+#include <openssl/sha.h>
+
+static value hxssl_sha1( value v ) {
+
+	unsigned char digest[SHA_DIGEST_LENGTH];
+	char* s = (char*)val_string(v);
+ 
+	SHA_CTX ctx;
+	SHA1_Init( &ctx );
+	SHA1_Update( &ctx, s, strlen(s) );
+	SHA1_Final( digest, &ctx );
+ 
+	char md[SHA_DIGEST_LENGTH*2+1];
+    for( int i = 0; i < SHA_DIGEST_LENGTH; i++ )
+		sprintf( &md[i*2], "%02x", (unsigned int)digest[i] );
+   
+    return alloc_string( md );
+}
+
+DEFINE_PRIM( hxssl_sha1, 1 );
---
+++ sys/crypto/SHA1.hx
@@ -0,0 +1,26 @@
+package sys.crypto;
+
+#if cpp
+import cpp.Lib;
+#else
+import neko.Lib;
+#end
+
+/**
+	SHA-1 (Secure Hash Algorithm)
+*/
+class SHA1 {
+
+	public static inline var DIGEST_LENGTH = 20;
+	
+	public static inline function encode( s : String ) : String {
+		#if cpp
+		return _sha1( s );
+		#elseif neko
+		return Lib.nekoToHaxe( _sha1( Lib.haxeToNeko(s) ) );
+		#end
+	}
+	
+	static inline function _sha1( s : String ) { return Lib.load( "ssl", "hxssl_sha1", 1 )(s); }
+	
+}
