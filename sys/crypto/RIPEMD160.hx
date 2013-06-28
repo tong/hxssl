585722218cf99fe26e999b1bed7c00f655cb2d36
tong <tong@disktree.net>
2013-06-28 15:59:13 +0200

    added ripemd

---
+++ sys/crypto/RIPEMD160.hx
@@ -0,0 +1,24 @@
+package sys.crypto;
+
+#if cpp
+import cpp.Lib;
+#else
+import neko.Lib;
+#end
+
+/**
+	RIPEMD-160
+*/
+class RIPEMD160 {
+	
+	public static inline function encode( s : String ) : String {
+		#if cpp
+		return _ripemd160( s );
+		#elseif neko
+		return Lib.nekoToHaxe( _ripemd160( Lib.haxeToNeko(s) ) );
+		#end
+	}
+	
+	static inline function _ripemd160( s : String ) { return Lib.load( "ssl", "hxssl_ripemd160", 1 )(s); }
+	
+}
