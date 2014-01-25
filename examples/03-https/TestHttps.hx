6743454aecbf271baaefaf02196f5ee7132e73f7
Sam MacPherson <xylom44@gmail.com>
2014-01-25 15:36:16 -0500

    removed custom cert location from examples

--- examples/03-https/TestHttps.hx
+++ examples/03-https/TestHttps.hx
@@ -7,8 +7,6 @@ class TestHttps {
 	
 	static function main() {
 		var r = new haxe.Http( URL );
-		r.certFolder = '/etc/ssl/certs';
-		r.certFile = '/etc/ssl/certs/ca-certificates.crt';
 		r.onData = println;
 		r.onError = println;
 		r.request();
