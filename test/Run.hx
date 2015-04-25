52f9109744b9c1f46b88239d6350576907e280fe
Sam MacPherson <xylom44@gmail.com>
2015-04-25 13:18:25 -0400

    re-add broken unit tests

--- test/Run.hx
+++ test/Run.hx
@@ -5,6 +5,11 @@ class Run {
 
 		var r = new haxe.unit.TestRunner();
 		
+		r.add( new TestBase64() );
+		r.add( new TestMD5() );
+		r.add( new TestSHA1() );
+		r.add( new TestSHA256() );
+		r.add( new TestRIPEMD160() );
 		#if !cpp
 		//FIXME https is broken for cpp in Haxe until this commit: https://github.com/HaxeFoundation/haxe/commit/b8a98987586a83900db54463eee66299c715cfb9
 		r.add( new TestHttp() );
