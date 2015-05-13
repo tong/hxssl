91c1e66bd22c64c2f157a38aaaac6ffcf8bb1705
Sam MacPherson <xylom44@gmail.com>
2015-05-13 16:06:16 -0400

    bump haxe version to 3.2.0; re-add http tests to hxcpp

--- test/Run.hx
+++ test/Run.hx
@@ -10,10 +10,7 @@ class Run {
 		r.add( new TestSHA1() );
 		r.add( new TestSHA256() );
 		r.add( new TestRIPEMD160() );
-		#if !cpp
-		//FIXME https is broken for cpp in Haxe until this commit: https://github.com/HaxeFoundation/haxe/commit/b8a98987586a83900db54463eee66299c715cfb9
 		r.add( new TestHttp() );
-		#end
 		r.add( new TestSocket() );
 		
 		if (r.run()) {
