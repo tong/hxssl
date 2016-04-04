b7bfc54c39e1c6382f61e649e82f92c9cf9eaee2
Tong <tong@disktree.net>
2016-04-04 02:35:03 +0200

    Re-enabled Base64 unit test

--- test/Run.hx
+++ test/Run.hx
@@ -1,24 +1,23 @@
 
 class Run {
-	
+
 	static function main() {
 
 		var r = new haxe.unit.TestRunner();
-		
-		//TODO fix Base64 on OS X -- getting seg faults
-		//r.add( new TestBase64() );
+
+		r.add( new TestBase64() );
 		r.add( new TestMD5() );
 		r.add( new TestSHA1() );
 		r.add( new TestSHA256() );
 		r.add( new TestRIPEMD160() );
 		r.add( new TestHttp() );
 		r.add( new TestSocket() );
-		
+
 		if (r.run()) {
 			Sys.exit(0);
 		} else {
 			Sys.exit(1);
 		}
 	}
-	
+
 }
